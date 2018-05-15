5cfb09a2fac8198bf4f8c9cf8de6ba8c4580f5af
Wyll Brimacombe <wyll.brimacombe@varsitytutors.com>
2018-05-15 10:49:45 -0700

    microposts - image import is incomplete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
 	def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -61,6 +62,26 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+    # Before filters
 
     # Confirms the correct user.
     def correct_user
